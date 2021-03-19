e1b1f1f369404b48b1c7ae0ac78919c4a3f3898f
Vic Istomin <vic.istomin@gmail.com>
2021-03-19 19:48:20 +0300

    Added support for 'Ё' letter in RU language

--- shift_cipher.py
+++ shift_cipher.py
@@ -1,27 +1,58 @@
 mode = int(input("Enter mode\n1: cipher\n2: decipher\n"))
-lang = int(input("Enter language\n1: RU (32 letters)\n2: EN\n"))
+lang = int(input("Enter language\n1: RU (33 letters)\n2: EN\n"))
 word = input("Enter message: ").lower()
 key = int(input("Enter key: "))
 
 
-def shift(word, key, module, start):
-    new_word = ''
+def shift(index, key, module):
+    encrypted_index = []
     for i in range(len(word)):
-        new_word += chr((((ord(word[i]) - start) + key) % module) + start)
-    return new_word
+        encrypted_index.append(((index[i]) + key) % module)
+    return encrypted_index
 
+
+indexes = []
+jo_shift = ord('ё') - ord('е') - 1
 
 if lang == 1:
-    module = 32
+    module = 33
     start = ord('а')
+
+    for i in range(len(word)):
+        index = ord(word[i])
+        if index == ord('ё'):
+            indexes.append(index - jo_shift - start)
+        elif index <= ord('е'):
+            indexes.append(index - start)
+        elif index > ord('е'):
+            indexes.append(index - start + 1)
+
 elif lang == 2:
     module = 26
     start = ord('a')
+    for i in range(len(word)):
+        indexes.append(ord(word[i]) - start)
 
 if mode == 2:
     key = 0 - key
 
-answer = shift(word, key, module, start)
+answer = ''
+
+new_indexes = shift(indexes, key, module)
+e_shift = ord('е')-ord('а')
+
+if lang == 1:
+    for i in range(len(new_indexes)):
+        if new_indexes[i] == e_shift + 1:  # Ё
+            answer += chr(new_indexes[i] + jo_shift + start)
+        elif new_indexes[i] <= e_shift:
+            answer += chr(new_indexes[i] + start)
+        elif new_indexes[i] > e_shift:
+            answer += chr(new_indexes[i] + start - 1)
+
+elif lang == 2:
+    for i in range(len(new_indexes)):
+        answer += chr(new_indexes[i] + start)
 
 if mode == 1:
     print(answer.upper())
